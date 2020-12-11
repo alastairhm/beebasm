f9aac096dec88595f0eb253c616f923c39a47d12
Alastair Montgomery <amontgomery@williamhill.co.uk>
2020-12-11 14:45:43 +0000

    10 Print in 6502 with custom characters

---
+++ examples/10print.asm
@@ -0,0 +1,53 @@
+\ 10 PRINT
+\ Using BeebASM assembler
+\ (c) Alastair Montgomery
+
+INCLUDE "../lib/constants.asm"
+screen = &7C00
+seed = &70
+bitmask = &72
+
+ORG &2000
+
+.start
+.mode
+    LDA #0
+    JSR screenmode
+    LDA #23
+    STA seed
+    LDA #23
+    STA seed+1
+    LDA #1
+    STA bitmask
+.char
+    LDA chartable, X
+    JSR oswrch
+    INX
+    CPX #21
+    BNE char
+.loop
+    JSR random
+    STA bitmask
+    LDA #1
+    BIT bitmask
+    BMI two
+.one
+    LDA #224
+    JMP print
+.two
+    LDA #225
+.print
+    JSR oswrch
+    JMP loop
+.finish
+    RTS
+.screenmode
+    INCLUDE "../lib/screenmode.asm"
+.random
+    INCLUDE "../lib/random.asm"
+.chartable
+    EQUB 23,224,8,8,12,7,224,48,16,16
+    EQUB 23,225,16,16,48,224,7,12,8,8
+.end
+
+SAVE "MyCode", start, end
